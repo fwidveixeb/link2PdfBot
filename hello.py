455924c1837334525b8e4e2544ee3ef51390c6bd
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-11 11:20:06 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/users/<string:username>')
 def hello_world(username='MyName'):
     return("Hello {}!".format(username))
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=81)
